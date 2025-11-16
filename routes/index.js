1d4f98e0efd1e8acb869dabfb7b4c0038363210d
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-11-16 23:50:03 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const router = require('express').Router();
 
-// Swagger UI at /api-docs
 router.use('/api-docs', require('./swagger'));
 
-// Root route
 router.get('/', (req, res) => { 
   //#swagger.tags = ['Hello World']
   res.send('Hello World!');
