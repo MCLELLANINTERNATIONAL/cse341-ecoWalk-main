01c8f5b60eb6dd28d3d24e9af0dcbec4445f1a81
MCLELLANINTERNATIONAL <bro24002@byui.edu>
2025-11-18 11:55:03 +0000

    update

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'ecoWalk API',
         description: 'API documentation for coastal hikes and trails'
     },
-    host: 'localhost:300',
+    host: 'localhost:3005',
     schemes: ['http', 'https'],
 };
 
